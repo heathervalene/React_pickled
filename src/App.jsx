fda69fc03551582a2b8d749a8dcbfb88509baf0f
HeatherRouke <heather214@gmail.com>
2024-03-11 12:34:01 -0400

    add emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 
 import './App.css'
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Recipe from './components/Recipe'
 import RecipeList from './components/RecipeList'
 import {Routes, Route} from "react-router-dom";
 import recipesArray from '../data/recipes'
 import About from './components/About'
 import Nav from './components/Nav';
+import AddRecipe from './components/AddRecipe';
 
 
 function App() {
 
 const [recipes, setRecipes] = useState(recipesArray)
 
-const navigate = useNavigate()
-
- 
 
   return (
 
@@ -28,6 +25,7 @@ const navigate = useNavigate()
         <h1 className='header'>Pickled</h1>
         <Routes>
           <Route path='/about' element={<About />} />
+          <Route path='/recipes/new' element={<AddRecipe />} />
         <Route path='/' element={<RecipeList recipes={recipes} />} />
         <Route path='/recipes/:index' element={<Recipe recipes={recipes} />}/>
         </Routes>
---
+++ src/components/AddRecipe.jsx
@@ -0,0 +1,13 @@
+
+
+const AddRecipe = () => {
+    return (
+        <div className ="container">
+        <h1>Add Recipe</h1>
+        <form>
+        </form>
+        </div>
+    )
+}
+
+export default AddRecipe;
