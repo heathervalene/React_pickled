8ae846d0a9f0b5b155de2994320830e62b456acb
HeatherRouke <heather214@gmail.com>
2024-03-11 11:24:04 -0400

    add about and nav components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Recipe from './components/Recipe'
 import RecipeList from './components/RecipeList'
 import {Routes, Route} from "react-router-dom";
 import recipesArray from '../data/recipes'
+import About from './components/About'
 
 
 function App() {
@@ -19,6 +20,7 @@ const [recipes, setRecipes] = useState(recipesArray)
         <main>
         <h1 className='header'>Pickled</h1>
         <Routes>
+          <Route path='/about' element={<About />} />
         <Route path='/' element={<RecipeList recipes={recipes} />} />
         <Route path='/recipes/:index' element={<Recipe recipes={recipes} />}/>
         </Routes>
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1>About</h1>
+            <p>This is a recipe app that allows users to create and view their own recipes. It was created using React, React Router, and Materialize.</p>
+        </div>
+    )
+
+}
+
+export default About;
